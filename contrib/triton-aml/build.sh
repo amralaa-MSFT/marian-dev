cc5175fc340def235b0c71da35db3aa54aef31db
Amr Alaa <amralaa@microsoft.com>
2021-06-13 16:31:41 +0000

    2021-06-13T16:31:41+00:00

---
+++ contrib/triton-aml/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -xv
+set -eu
+
+echo Building Triton Marian backend ...
+
+image_name='triton-marian-builder:20.09'
+docker build -t "$image_name" --build-arg CACHEBUST="$(git rev-parse HEAD)" .
+
+echo Copying artifacts ...
+
+docker container create --name extract "$image_name"
+docker container cp extract:/opt/tritonserver/marian_backend/build/libtriton_marian.so .
+docker container rm -f extract
