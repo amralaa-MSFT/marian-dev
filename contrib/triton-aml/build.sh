0febb25a6ab2aa95d8b44fae55b6c72e75735e24
Amr Alaa <amralaa@microsoft.com>
2021-06-13 13:03:01 +0000

    Build debug

--- contrib/triton-aml/build.sh
+++ contrib/triton-aml/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo Building Triton Marian backend ...
 
-docker build -t triton-marian-build .
+docker build -t triton-marian-builder-only:debug .
 
 echo Copying artifacts ...
 
-docker container create --name extract triton-marian-build
-docker container cp extract:/opt/tritonserver/marian_backend/build/libtriton_marian.so .
+docker container create --name extract triton-marian-builder-only:debug
+docker container cp extract:/opt/tritonserver/marian_backend/build/libtriton_marian.so ./libtriton_marian.latest-debug.so
 docker container rm -f extract
