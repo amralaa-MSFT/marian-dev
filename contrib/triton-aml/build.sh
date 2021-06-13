c6df8804be95423a540b6fcd4b9b5bc4231e16c4
Amr Alaa <amralaa@microsoft.com>
2021-06-13 14:41:29 +0000

    Update nvidia image

--- contrib/triton-aml/build.sh
+++ contrib/triton-aml/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 echo Building Triton Marian backend ...
 
-docker build -t triton-marian-builder-only:debug .
+image_name='triton-marian-builder-only:21.09-debug'
+docker build -t "$image_name" .
 
 echo Copying artifacts ...
 
-docker container create --name extract triton-marian-builder-only:debug
+docker container create --name extract "$image_name"
 docker container cp extract:/opt/tritonserver/marian_backend/build/libtriton_marian.so ./libtriton_marian.latest-debug.so
 docker container rm -f extract
